fa7b88fc622e6f75080cf31ee1a37590c9378ca1
Muhammad Hamza <muhammadhamzajanjua786@gmail.com>
2025-09-20 23:44:45 +0100

    hi

--- app/src/main/java/com/app/simplesum/MainActivity.kt
+++ app/src/main/java/com/app/simplesum/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        // Hi
 //        enableEdgeToEdge()
         setContentView(binding.root)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
