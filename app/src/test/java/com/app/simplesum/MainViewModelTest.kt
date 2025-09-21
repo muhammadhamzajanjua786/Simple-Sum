ead754aa0fa56859ffcfe72b63fff77d3d2d1520
Muhammad Hamza <muhammadhamzajanjua786@gmail.com>
2025-09-21 11:02:15 +0100

    run failed test cases

--- app/src/test/java/com/app/simplesum/MainViewModelTest.kt
+++ app/src/test/java/com/app/simplesum/MainViewModelTest.kt
@@ -32,7 +32,7 @@ class MainViewModelTest {
         viewModel.setSecondNumber("3")
         viewModel.onCalculateClick()
 
-        assertThat(viewModel.sum.value).isEqualTo("5")
+        assertThat(viewModel.sum.value).isEqualTo("4")
     }
 
     @Test
